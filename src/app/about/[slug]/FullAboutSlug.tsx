80ff2a1a7155b962f889a2a6ffd89aaf90d98bc7
Daniil Smagin <gargon38rus1@gmail.com>
2025-09-07 04:45:57 +0300

    c21

--- src/app/about/[slug]/FullAboutSlug.tsx
+++ src/app/about/[slug]/FullAboutSlug.tsx
@@ -5,15 +5,15 @@ import { Dasha } from "./Dasha"
 
 
 export function FullAboutSlug() {
-    const param = useParams<{ slug: string }>()
-    if (param.slug === 'choreographer') {
+    const {param} = useParams()
+    if (param === 'choreographer') {
         return (
             <div>
                 <Dasha/>
             </div>
         )
     }
-    if (param.slug === "director") {
+    if (param === "director") {
         return (
         <div>
             <h1>Директор</h1>
