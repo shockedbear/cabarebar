8755997f554a1854c3e645ab6462a15e1b7c66fe
Daniil Smagin <gargon38rus1@gmail.com>
2025-09-11 16:29:48 +0300

    finis21

--- src/app/afisha/booking/[id]/page.tsx
+++ src/app/afisha/booking/[id]/page.tsx
@@ -1,23 +1,18 @@
-import { EVENTSINFOALL } from "@/data/events.all.data"
 
 export function generateStaticParams() {
-  return EVENTSINFOALL.map(event => ({
-    id: event.id
-  }))
+  return (
+    {id: "1"}
+  )
 }
 
 export default async function Booking({params}: {params: Promise<{ id: string }>})  {
   const {id} = await params
 
-  const event = EVENTSINFOALL.find(e => e.id === id)
-  if (!event) return <p>Событие не найдено</p>
+  if (id === '1') {
+    return(
+      <span data-tc-event-inline="68c19087f1f567b1d7d091f8" data-tc-token="eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhjMDM0MjAxZTM3MjJlZDM0MzM4ZjQ4In0.iSIxulT2fYCyWpgCGEgLuA2DkQkWH51szZYSu1Y9aE0"></span>
+    )
+  }
 
-  return (
-    <div >
-      <span
-        data-tc-event-inline={event.dataTcEvent}
-        data-tc-token={event.dataTcToken}
-      ></span>
-    </div>
-  )
+  
 }
