a3fadba18c38201ed210492743b95f5d43b48c1b
Daniil Smagin <gargon38rus1@gmail.com>
2025-09-12 02:23:59 +0300

    wqeqas

--- src/app/bar/SectionImageBar.tsx
+++ src/app/bar/SectionImageBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 const slides = [
-  { type: "image", src: "/images/bar/barM1.jpg" },
+  { type: "image", src: "/images/bar/BarM1.jpg" },
      
 
 ];
